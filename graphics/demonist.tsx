6ba15f6e68d620ffb820013481746e62369b21ed
d0pelrh <realhllcngn@gmail.com>
2024-09-01 15:18:59 +0200

    more stone wall sprites

--- graphics/demonist.tsx
+++ graphics/demonist.tsx
@@ -1,22 +1,46 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="demonist" tilewidth="12" tileheight="24" tilecount="6" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="demonist" tilewidth="12" tileheight="24" tilecount="14" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="3">
+ <tile id="40">
+  <image width="12" height="24" source="Sprite-0000.png"/>
+ </tile>
+ <tile id="35">
   <image width="12" height="24" source="Sprite-0001.png"/>
  </tile>
- <tile id="4">
+ <tile id="36">
   <image width="12" height="24" source="Sprite-0002.png"/>
  </tile>
  <tile id="29">
   <image width="12" height="24" source="Sprite-0003.png"/>
  </tile>
- <tile id="30">
-  <image width="12" height="24" source="Sprite-0005.png"/>
- </tile>
  <tile id="31">
   <image width="12" height="24" source="Sprite-0004.png"/>
  </tile>
+ <tile id="30">
+  <image width="12" height="24" source="Sprite-0005.png"/>
+ </tile>
  <tile id="32">
   <image width="12" height="24" source="Sprite-0006.png"/>
+ </tile>
+ <tile id="37">
+  <image width="12" height="24" source="Sprite-0007.png"/>
+ </tile>
+ <tile id="41">
+  <image width="12" height="24" source="Sprite-0010.png"/>
+ </tile>
+ <tile id="42">
+  <image width="12" height="24" source="Sprite-0011.png"/>
+ </tile>
+ <tile id="38">
+  <image width="12" height="24" source="Sprite-0008.png"/>
+ </tile>
+ <tile id="39">
+  <image width="12" height="24" source="Sprite-0009.png"/>
+ </tile>
+ <tile id="47">
+  <image width="12" height="24" source="Sprite-0013.png"/>
+ </tile>
+ <tile id="51">
+  <image width="12" height="24" source="Sprite-0012.png"/>
  </tile>
 </tileset>
